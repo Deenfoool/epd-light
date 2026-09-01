91cb4104af4eefc1e80f9bfda9bed45a98b4e928
Денис Маняхин <salumbek.den@gmail.com>
2026-09-01 12:31:56 +0300

    style: load draft-v2 layout overrides

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './official-links.css'
+import './v2.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode><App /></React.StrictMode>,
